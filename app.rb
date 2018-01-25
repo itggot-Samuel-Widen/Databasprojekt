d8a4d3d428757dee0b4801c399fdfee38d9655e4
Samuel Widén <samuel.widen@learnet.se>
2018-01-25 10:36:50 +0100

    Började på registreringen och login och ändrade databasnamn

--- app.rb
+++ app.rb
@@ -4,5 +4,27 @@ class App < Sinatra::Base
 		slim(:index)
 	end
 
-	test 3000
+	get '/publish' do
+		slim(:publish)
+	end
+
+	post '/publish' do
+		db = SQLite3::Database.new("./db/databasprojekt.sqlite")
+		description = params[:description]
+		tags = params[:tags]
+		content = params[:content]
+		db.execute("INSERT INTO uploads (description, tags, content) VALUES(?,?,?)", [description, tags, content])
+	end
+
+	get '/login' do
+		slim (:login)
+	end
+
+	get '/register' do
+		slim (:register)
+	end
+
+	post '/register' do
+		db = SQLite3::Database.new("./db/databasprojekt.sqlite")
+	end
 end
